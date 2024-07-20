a7639307bd71c04d9d81fbd072a5b2b408452c2a
Cagdas MARNAGOZ <marangoz.1697@gmail.com>
2024-07-20 12:54:51 +0300

    high score added

--- app/src/main/java/com/cagdasmarangoz/tasbeehapp/MainActivity.kt
+++ app/src/main/java/com/cagdasmarangoz/tasbeehapp/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.cagdasmarangoz.tasbeehapp
 
+import android.content.Context
 import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.cagdasmarangoz.tasbeehapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
-    var count=0
+    lateinit var binding: ActivityMainBinding
+    var count = 0
+    var highScorer = 0
     var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,16 +18,51 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.apply {
+            val sharedPreferences = getSharedPreferences("sharedpref", Context.MODE_PRIVATE)
+            val saveScore = sharedPreferences.getString("STRING_KEY", null)
+         highScorer = saveScore!!.toInt()
+
+            highScore.text = highScorer.toString()
             pushButton.setOnClickListener {
+
                 count++
                 mediaPlayer = MediaPlayer.create(this@MainActivity, R.raw.alert)
                 mediaPlayer?.start()
-                numberText.text=count.toString()
+                numberText.text = count.toString()
+                highScorer++
+                highScore.text = highScorer.toString()
+
+                saveData(highScorer.toString())
+
+
+
+
             }
+
+
             rButton.setOnClickListener {
-                count=0
-                numberText.text=count.toString()
-            }
+                count = 0
+                numberText.text = count.toString()
+
+
         }
+
+    }
+
+        loadData()
+    }
+
+    private fun saveData(value:String) {
+        val sharedPreferences = getSharedPreferences("sharedpref", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("STRING_KEY", value)
+        editor.commit()
+    }
+
+    private fun loadData() {
+        val sharedPreferences = getSharedPreferences("sharedpref", Context.MODE_PRIVATE)
+        val saveScore = sharedPreferences.getString("STRING_KEY", null)
+        binding.highScore.text = saveScore.toString()
+
     }
 }
